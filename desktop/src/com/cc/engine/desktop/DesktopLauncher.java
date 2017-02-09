92c6da34a57ef0c1221ebeb98855a0d0dba630cd
Jon <jmrapp1270@gmail.com>
2017-02-09 15:33:33 -0500

    Initial

---
+++ desktop/src/com/cc/engine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cc.engine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cc.engine.EngineMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EngineMain(), config);
+	}
+}
---
+++ html/src/com/cc/engine/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.cc.engine.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.cc.engine.EngineMain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new EngineMain();
+        }
+}

837871f490ed45dd58c4f62e8f89107d4a2419f4
Jon <jmrapp1270@gmail.com>
2017-02-09 19:35:52 -0500

    Boilerplate added to Main

--- core/src/com/cc/engine/EngineMain.java
+++ core/src/com/cc/engine/EngineMain.java
@@ -2,32 +2,82 @@ package com.cc.engine;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.cc.engine.utils.Settings;
+import com.cc.engine.utils.Timer;
+import com.cc.engine.views.drawables.ResourceManager;
+import com.cc.engine.views.screens.ScreenManager;
 
 public class EngineMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	protected SpriteBatch sb;
+	protected boolean paused;
+	protected long lastBack;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		sb = new SpriteBatch(150);
+		sb.enableBlending();
+		instantiateSettings();
+		Gdx.input.setCatchBackKey(true); //Stops from exiting when back button pressed
+
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		if (!paused) {
+			Timer.getInstance().update();
+
+			if (ScreenManager.getCurrent() != null)
+				ScreenManager.getCurrent().update();
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.BACK) && System.currentTimeMillis() - lastBack > 300) {
+			if (ScreenManager.getCurrent() != null) {
+				ScreenManager.getCurrent().onBackPressed();
+			}
+			lastBack = System.currentTimeMillis();
+		}
+
+		Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl20.glClearColor(0f, 0f, 0f, 1);
+
+		if (ScreenManager.getCurrent() != null) {
+			ScreenManager.getCurrent().render(sb);
+		}
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void resize(int width, int height) {
+		if (ScreenManager.getCurrent() != null)
+			ScreenManager.getCurrent().resize(width, height);
 	}
+
+	@Override
+	public void pause() {
+		paused = true;
+		if (ScreenManager.getCurrent() != null)
+			ScreenManager.getCurrent().pause();
+	}
+
+	@Override
+	public void resume() {
+		paused = false;
+		if (ScreenManager.getCurrent() != null)
+			ScreenManager.getCurrent().resume();
+	}
+
+	@Override
+	public void dispose() {
+		sb.dispose();
+		ResourceManager.getInstance().dispose();
+	}
+
+	public static void instantiateSettings() {
+		Settings.addSetting("width", 800);
+		Settings.addSetting("height", 480);
+	}
+
 }

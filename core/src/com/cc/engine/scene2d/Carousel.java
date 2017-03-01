0bb3ed36c242f3189df94afbcc35b7a0d9efbb51
cody <codyallenbrooks12@yahoo.com>
2017-02-28 19:50:25 -0800

    Circles are drawn to indicate the current page of the carousel. Calculate the current page index by the most visible page.

--- core/src/com/cc/engine/scene2d/Carousel.java
+++ core/src/com/cc/engine/scene2d/Carousel.java
@@ -1,6 +1,8 @@
 package com.cc.engine.scene2d;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Cell;
@@ -13,6 +15,8 @@ import com.badlogic.gdx.utils.Array;
  * Created by cody on 2/16/17.
  */
 public class Carousel extends ScrollPane {
+
+    private ShapeRenderer shapeRenderer;
 
     private boolean wasPanDragFling = false;
 
@@ -46,6 +50,7 @@ public class Carousel extends ScrollPane {
         content = new Table();
         content.defaults().space(50);
         super.setWidget(content);
+        shapeRenderer = new ShapeRenderer();
     }
 
     public void addPages(Actor... pages) {
@@ -108,13 +113,11 @@ public class Carousel extends ScrollPane {
         float pageX = 0;
         float pageWidth = 0;
         if (pages.size > 0) {
-            for (int i = 0; i < pages.size; i ++)
-            {
+            for (int i = 0; i < pages.size; i++) {
                 Actor a = pages.get(i);
                 pageX = a.getX();
                 pageWidth = a.getWidth();
                 if (scrollX < (pageX + pageWidth * 0.5)) {
-                    currentPageIndex = i;
                     break;
                 }
             }
@@ -122,15 +125,39 @@ public class Carousel extends ScrollPane {
         }
     }
 
-    public Array<Actor> getPages () {
+    @Override
+    public void setScrollX(float pixels) {
+        super.setScrollX(pixels);
+        final float width = getWidth();
+        final float scrollX = getScrollX();
+        float pageX = 0;
+        float pageWidth = 0;
+        float visibility = 0;
+        float lowestVisibility = 9999;
+        int visibilityIndex = 0;
+        Array<Actor> pages = getPages();
+        for (int i = 0; i < pages.size; i++) {
+            Actor page = pages.get(i);
+            pageX = page.getX();
+            pageWidth = page.getWidth();
+            visibility = Math.abs((scrollX + pageWidth / 2) - pageX);
+            if (visibility <= lowestVisibility) {
+                lowestVisibility = visibility;
+                visibilityIndex = i;
+            }
+        }
+        currentPageIndex = visibilityIndex;
+    }
+
+    public Array<Actor> getPages() {
         return content.getChildren();
     }
 
-    public int getCurrentPageIndex () {
+    public int getCurrentPageIndex() {
         return currentPageIndex;
     }
 
-    public void setCurrentPageIndex (int pageIndex) {
+    public void setCurrentPageIndex(int pageIndex) {
         Array<Actor> pages = getPages();
         final float scrollX = getScrollX();
         Actor page = pages.get(pageIndex);
@@ -143,10 +170,25 @@ public class Carousel extends ScrollPane {
     }
 
     @Override
-    public void draw(Batch batch, float alpha)
-    {
+    public void draw(Batch batch, float alpha) {
         super.draw(batch, alpha);
         // TODO: Create way to show what the current page is
+        Array<Actor> pages = getPages();
+        int currentPageIndex = getCurrentPageIndex();
+        float x, y = getY() + 15, radius = 10;
+        float widthOfAllCircles = (pages.size - 1) * (radius * 2);
+        for (int i = 0; i < pages.size; i++) {
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            if (currentPageIndex == i) {
+                shapeRenderer.setColor(Color.GRAY);
+            } else {
+                shapeRenderer.setColor(Color.BLACK);
+            }
+            x = getX() + (getWidth() / 2) - (widthOfAllCircles / 2);
+            x += (i * radius * 2);
+            shapeRenderer.circle(x, y, radius);
+            shapeRenderer.end();
+        }
     }
 
 }

6d94055e190c2b23f3373eadafe1a865e55178cf
Jon <jmrapp1270@gmail.com>
2017-03-09 21:03:46 -0500

    Added achievement handler

---
+++ core/src/com/cc/engine/gpgs/IAchievementHandler.java
@@ -0,0 +1,13 @@
+package com.cc.engine.gpgs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jon
+ * Date: 3/9/2017
+ * Time: 8:41 PM
+ */
+public interface IAchievementHandler {
+
+    void unlockAchievement(String id);
+
+}
